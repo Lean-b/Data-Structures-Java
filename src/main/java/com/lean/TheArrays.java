77b50c6534f80fae118db1374bd78803fa51ab4c
Lean-b <leandroborqueza@gmail.com>
2023-07-09 11:33:56 -0300

    data structures arrays

--- src/main/java/com/lean/TheArrays.java
+++ src/main/java/com/lean/TheArrays.java
@@ -18,6 +18,7 @@ public class TheArrays {
 
         System.out.println();
 
+        // Crea un array
         String[] colors = new String[5];
         colors[0] = "purple";
         colors[1] = "blue";
@@ -42,52 +43,53 @@ public class TheArrays {
         }
 
         System.out.println();
-
+        // Itera sobre el array colors y asigna cada elemento a la variable "color"
         for (int i = 0; i < colors.length; i++) {
             System.out.println(colors[i]);
         }
 
         System.out.println();
-
+        // Itera sobre el array colors en orden inverso utilizando un bucle for
         for (int i = colors.length - 1; i >= 0; i--) {
             System.out.println(colors[i]);
         }
 
         System.out.println();
 
+        // Convierte el array colors en un flujo de elementos y los imprime en la consola utilizando una referencia de método
         Arrays.stream(colors).forEach(System.out::println);
 
         System.out.println();
 
+        // Convierte el array colors en un flujo de elementos y los imprime en la consola utilizando una expresión lambda.
         Arrays.stream(colors).forEach(color -> System.out.println(color));
 
         System.out.println();
 
+        // Convierte el array colors en un flujo de elementos y los imprime en la consola utilizando una expresión lambda.
+        // Y filtra los elementos que comienzan con "b".
         Arrays.stream(colors).filter(color -> color.startsWith("b")).forEach(System.out::println);
 
         System.out.println();
 
-        Arrays.stream(colors).forEach(System.out::println);
-
-        System.out.println();
-
+        // Imprime el contenido del array colors en paralelo utilizando un flujo de elementos(lo hace de forma desordenada).
         Arrays.stream(colors).parallel().forEach(System.out::println);
         //Arrays.stream(colors).parallel().forEach(System.out::println);
 
         System.out.println();
 
+        // Itera sobre el flujo de elementos del array colors utilizando un bucle for y el método count() para determinar la cantidad de elementos
         for (int i = 0; i < Arrays.stream(colors).count(); i++) {
             System.out.println(colors[i]);
         }
 
-        System.out.println();
-        System.out.println();
         System.out.println();
 
         System.out.println("Number");
 
         System.out.println();
 
+        // Crea un array de tipo int con capacidad para 5 elementos
         int[] number = new int[5];
         number[0] = 0;
         number[1] = 1;
@@ -100,7 +102,7 @@ public class TheArrays {
 
         System.out.println();
 
-
+        // Otra forma de crear  un array
         int[] numbers = {1, 2, 3, 4, 5};
 
         Arrays.stream(numbers).forEach(System.out::println);
